e495c8df4074f3d040a4dd5a18f9eb6c596aa48e
Zach Smith <zsmith126028@gmail.com>
2018-11-13 14:57:17 -0600

    add minutes to setDate function

--- JavaScript30-master/02 - JS and CSS Clock/script.js
+++ JavaScript30-master/02 - JS and CSS Clock/script.js
@@ -1,16 +1,19 @@
-// const secondHand = document.querySelector('.second-hand');
-
 function setDate() {
   const secondHand = document.querySelector('.second-hand');
-
+  const minuteHand = document.querySelector('.min-hand');
 
   const now = new Date();
 
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
   console.log(seconds);
+  console.log(minutes);
 }
 
 setInterval(setDate, 1000);
